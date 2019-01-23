f42ff00da1b78ea6aadd5c7c3203ff17d95facb3
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2019-01-23 09:25:12 -0700

    Commit inicial

---
+++ js/index.js
@@ -0,0 +1,45 @@
+Splitting();
+
+/* COUNTER */
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+var totalSeconds = 0;
+
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  document.getElementById("seconds").innerHTML = pad(totalSeconds % 60);
+  document.getElementById("minutes").innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+/* TIME IN FORMAT HH:MM:SS */
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+setInterval(getTime, 1000);
+
+function getTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers < 10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout(function() {
+    getTime()
+  }, 500);
+}
